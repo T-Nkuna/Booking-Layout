18291174028a6c0253c6de0d3fa7acebe6e21fde
tnkuna253 <nkuna@etiocs.com>
2020-06-24 17:52:20 +0200

    Added fontawesome brand and solid icons to solution

--- js/index.js
+++ js/index.js
@@ -11,6 +11,19 @@ let leftOptions =[
     option.innerHTML = optionName;
     return option;
 });
-let topPanel = new TopPanel(leftOptions,[]);
+
+let rightOptions = [
+    "fab fa-facebook-f",
+    "fab fa-twitter",
+    "fab fa-pinterest-p","fab fa-instagram",
+    "fas fa-phone","fas fa-envelope"
+].map(iconClass=>{
+    let iconContainer = document.createElement("a");
+    let icon = document.createElement("i");
+    icon.className = iconClass;
+    iconContainer.appendChild(icon);
+    return iconContainer;
+})
+let topPanel = new TopPanel(leftOptions,rightOptions);
 appContainer.append(topPanel.createElement());
 
